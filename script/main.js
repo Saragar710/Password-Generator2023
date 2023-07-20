539610a14de2931e847d2ce0954b8390b7e23d56
Sara Garcia <saragca81@gmail.com>
2023-07-20 14:18:36 -0700

    added var to the javascript

--- script/main.js
+++ script/main.js
@@ -1,10 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var length=10;
+var upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase ="abcdefghijklmnopqrstuvwxyz";
+var numbers = "1234567890";
+var specialCharacters ="!@#$%^&*()?/";
+var allChar ="upperCase, lowerCase, numbers, specialCharacters";
 
+return #generate
+
+// function generatePassword() {
+//   console.log("Thanks for clicking the button!")
+// }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  var password = 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
