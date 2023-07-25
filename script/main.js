e38eb5d67188f5c812452743f857b98e52fe1bbd
Sara Garcia <saragca81@gmail.com>
2023-07-24 17:25:02 -0700

    reworked the code, started basically from beginning. understand more. a few more bugs to go

--- script/main.js
+++ script/main.js
@@ -1,49 +1,62 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var length=10;
+var characterLength=10;
 var upperCase ="A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
 var lowerCase ="a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
 var number = "1,2,3,4,5,6,7,8,9,0";
 var specialCharacter ="!,@,#,$,%,^,&,*,(,),?,/,";
 var allChar ="upperCase + lowerCase + number + specialCharacter";
-var length = 10;
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-
-  function generatePassword() {
-  function let(password) {("password")};
-  function let(password)  {("upperCase(Math.floor(Math.random)()*[upperCase.length]")};
-  function let(password) {("lowerCase(Math.floor(Math.random)()*[lowerCase.length]")};
-  function let(password)  {("number(Math.floor(Math.random)*[numbers.length]")};
-  function let(password)  {("specialCharacter(Math.floor(Math.random)*[specialCharacters]")};
-      // console.log(Math.floor(Math.random() *10);
-      var generatePassword = lowerCase + upperCase + number + specialCharacter
-
-
-
-  while(length > writePassword.length) 
-    password += allChar[Math.floor(Math.random()*allChar.length)];
-  
-  generatePassword.valueOf = password;
-  
-  //  for(var i=0; i <passwordLength. i ++;) {
-  //   var randomPassword = Math.floor(Math.random() * collectAllCharacters.length);
-  //   password += collectAllCharacters.CharAt(randomPassword);
-
-
-  //  }
-
-  }
-    // var password = 
-    var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-console.log("click", writePassword);
+generateBtn.addEventListener("click", writePassword) 
+
+// Write password to the #password input
+function writePassword () {
+  var correctPrompts = getPrompts();
+  var passwordText = document.querySelector("#password");
+
+
+  if(correctPrompts){
+  var password = generatePassword();
+  
+
+  passwordText.value = password;
+  } else {
+    passwordText.value = "";
+  }
+}
+ 
+ function generatePassword() {
+  var password = "";
+  for(var i = 0; i < characterLength; i++){
+    var randomLetter = Math.floor(Math.random() * allChar.length)
+    password = password + allChar[randomLetter]
+  }
+  return password;
+ }
+
+ function getPrompts() {
+   allChar = "";
+   characterLength = parseInt(prompt("How many characters do you want your password to be? (10 - 128 characters"));
+
+   if (isNaN(characterLength) || characterLength < 10 || characterLength > 128) {
+    alert("Please try again. Character length must be a number");
+    return false;
+   }
+ 
+    if (confirm("Do you want to use uppercase letters in your password?")){
+        allChar = allChar.concat("upperCase");
+    }
+    if (confirm("Do you want to use lowerCase letters in your password?")) {
+        allChar = allChar.concat("lowerCase");
+    }
+    if (confirm("Do you want to use any numbers in your password?")) {
+        allChar = allChar.concat("number");
+    }  
+    if (confirm("Do you want to use any special characters in your password?")) {
+        allChar = allChar.concat("specialCharacters");
+    }
+    return true;
+  }
